c5b5a2e170f39816ae08c263ced66b7313ac0cb7
Andy Park <apark1214@gmail.com>
2021-04-20 20:05:03 -0700

    completed testing

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 let $test = document.querySelector('.submitButton');
 let $header = document.querySelector('.header');
-let $body = document.querySelector('body')
+let $body = document.querySelector('body');
+let $testing = document.querySelector('.test')
 let status = false;
+
 
 $test.addEventListener('click', ()=>{
   if(status){
@@ -12,4 +14,8 @@ $test.addEventListener('click', ()=>{
     $body.classList.remove('changeBackground');
     status = true;
   }
+})
+
+$testing.addEventListener('click', ()=>{
+  console.log('hello')
 })
